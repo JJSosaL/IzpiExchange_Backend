e84f6e35ca2b6e2622b5e9c5d299d61f46f85b11
JJSosaL <hello@fancystudio.xyz>
2026-04-19 16:51:16 +0200

    Extraer la variable de entorno utilizando `getEnvironmentVariable`

--- prisma-next.config.ts
+++ prisma-next.config.ts
@@ -1,12 +1,10 @@
 import 'dotenv/config';
-import { env } from 'node:process';
 import MongoAdapter from '@prisma-next/adapter-mongo/control';
 import { defineConfig } from '@prisma-next/cli/config-types';
 import MongoDriver from '@prisma-next/driver-mongo/control';
 import { mongoFamilyDescriptor, mongoTargetDescriptor } from '@prisma-next/family-mongo/control';
 import { mongoContract } from '@prisma-next/mongo-contract-psl/provider';
-
-const { MONGO_DB_CONNECTION_URL } = env;
+import { getEnvironmentVariable } from '#utils/Process/getEnvironmentVariable.js';
 
 export default defineConfig({
 	adapter: MongoAdapter,
@@ -14,7 +12,7 @@ export default defineConfig({
 		output: 'src/contract.json',
 	}),
 	db: {
-		connection: MONGO_DB_CONNECTION_URL,
+		connection: getEnvironmentVariable('MONGO_DB_CONNECTION_URL'),
 	},
 	driver: MongoDriver,
 	family: mongoFamilyDescriptor,
