3a3ba383e56edaca9b16207c6705e1a471168482
JJSosaL <hello@fancystudio.xyz>
2026-04-19 20:23:58 +0200

    Añadir método `createPasswordHash`

--- src/modules/Auth/Auth.service.ts
+++ src/modules/Auth/Auth.service.ts
@@ -1,10 +1,23 @@
 import { randomInt } from 'node:crypto';
 import { Injectable } from '@nestjs/common';
+import { hash } from 'bcryptjs';
 
 @Injectable()
 export class AuthService {
+	private static BCRYPT_SALT_LENGTH = 12 as const;
+
 	private static OTP_MAX_LENGTH = 999999 as const;
 	private static OTP_MIN_LENGTH = 100000 as const;
+
+	/**
+	 * Crea un 'hash' de la contraseña introducida.
+	 *
+	 * @param password - La contraseña a 'hashear'.
+	 * @returns El 'hash' de la contraseña introducida.
+	 */
+	public async createPasswordHash(password: string): Promise<string> {
+		return await hash(password, AuthService.BCRYPT_SALT_LENGTH);
+	}
 
 	/**
 	 * Genera un código 'One-Time Password' para verificar la creación de la
