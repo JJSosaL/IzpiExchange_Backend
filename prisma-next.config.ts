14dad6a69af2cf0d886d4d8f5c1fccc2c1f8d804
JJSosaL <hello@fancystudio.xyz>
2026-04-19 17:21:37 +0200

    Modificar las constantes de configuración

--- prisma-next.config.ts
+++ prisma-next.config.ts
@@ -1,10 +1,9 @@
-import 'dotenv/config';
 import MongoAdapter from '@prisma-next/adapter-mongo/control';
 import { defineConfig } from '@prisma-next/cli/config-types';
 import MongoDriver from '@prisma-next/driver-mongo/control';
 import { mongoFamilyDescriptor, mongoTargetDescriptor } from '@prisma-next/family-mongo/control';
 import { mongoContract } from '@prisma-next/mongo-contract-psl/provider';
-import { getEnvironmentVariable } from '#utils/Process/getEnvironmentVariable.js';
+import { MONGO_DB_CONNECTION_URL } from '#root/config.js';
 
 export default defineConfig({
 	adapter: MongoAdapter,
@@ -12,7 +11,7 @@ export default defineConfig({
 		output: 'src/contract.json',
 	}),
 	db: {
-		connection: getEnvironmentVariable('MONGO_DB_CONNECTION_URL'),
+		connection: MONGO_DB_CONNECTION_URL,
 	},
 	driver: MongoDriver,
 	family: mongoFamilyDescriptor,
--- src/config.ts
+++ src/config.ts
@@ -3,8 +3,8 @@ import { getEnvironmentVariable } from '#utils/Process/getEnvironmentVariable.js
 /**
  * El dominio permitido para los correos electrónicos.
  *
- * Cualquier correo electrónico que no utilice este dominio será rechazado al
- * momento de intentar crear una cuenta nueva.
+ * Cualquier correo electrónico que no provenga de este dominio será rechazado
+ * al momento de intentar crear una cuenta nueva.
  */
 export const ALLOWED_EMAIL_DOMAIN = getEnvironmentVariable('ALLOWED_EMAIL_DOMAIN');
 
@@ -14,4 +14,38 @@ export const ALLOWED_EMAIL_DOMAIN = getEnvironmentVariable('ALLOWED_EMAIL_DOMAIN
  * NOTA: Si el valor del puerto se modifica, se deberá modificar también el
  * puerto dentro de la aplicación de Flutter en modo desarrollo.
  */
-export const PORT = 3001;
+export const APP_PORT = 3001 as const;
+
+/**
+ * El nombre del servidor de correo que se utilizará para enviar correos.
+ */
+export const EMAIL_HOST_NAME = 'smtp.protonmail.ch' as const;
+
+/**
+ * El puerto del servidor de correo que se utilizará para enviar correos.
+ */
+export const EMAIL_HOST_PORT = 587 as const;
+
+/**
+ * El usuario que se utilizará para autenticarse y enviar correos mediante el
+ * servidor de correo.
+ */
+export const EMAIL_USER_NAME = getEnvironmentVariable('EMAIL_USER_NAME');
+
+/**
+ * La contraseña que se utilizará para autenticarse y enviar correos mediante
+ * el servidor de correo.
+ */
+export const EMAIL_USER_TOKEN = getEnvironmentVariable('EMAIL_USER_TOKEN');
+
+/**
+ * La URL del servidor de MongoDB donde se guardarán todos los datos de la
+ * aplicación.
+ */
+export const MONGO_DB_CONNECTION_URL = getEnvironmentVariable('MONGO_DB_CONNECTION_URL');
+
+/**
+ * El nombre de la base de datos de MongoDB donde se guardarán todos los datos
+ * de la aplicación.
+ */
+export const MONGO_DB_DATABASE_NAME = getEnvironmentVariable('MONGO_DB_DATABASE_NAME');
