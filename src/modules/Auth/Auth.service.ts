9cc5a4dfd51c02712090ebe8e0c8015bea85b987
JJSosal <hello@fancystudio.xyz>
2026-04-21 12:00:57 +0200

    Eliminar TSDoc

--- src/modules/Auth/Auth.service.ts
+++ src/modules/Auth/Auth.service.ts
@@ -6,12 +6,6 @@ export class AuthService {
 	private static OTP_MAX_LENGTH = 999999 as const;
 	private static OTP_MIN_LENGTH = 100000 as const;
 
-	/**
-	 * Genera un código 'One-Time Password' para verificar cualquier acción
-	 * importante sobre las cuentas.
-	 *
-	 * @returns El código 'One-Time Password' como una cadena de texto.
-	 */
 	public generateOneTimePasswordCode(): string {
 		const otpInteger = randomInt(AuthService.OTP_MIN_LENGTH, AuthService.OTP_MAX_LENGTH);
 		const otpString = otpInteger.toString();
