5b7e69df16550d0632aba4ef3480612aca0b548f
JJSosaL <hello@fancystudio.xyz>
2026-04-21 23:26:50 +0200

    Eliminar documentos expirados después de cierto tiempo

--- src/modules/App.module.ts
+++ src/modules/App.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ScheduleModule } from '@nestjs/schedule';
 import { MONGO_DB_CONNECTION_URL, MONGO_DB_DATABASE_NAME } from '#root/config.js';
 import { AuthModule } from './Auth/Auth.module.js';
 
@@ -9,6 +10,7 @@ import { AuthModule } from './Auth/Auth.module.js';
 		MongooseModule.forRoot(MONGO_DB_CONNECTION_URL, {
 			dbName: MONGO_DB_DATABASE_NAME,
 		}),
+		ScheduleModule.forRoot(),
 	],
 })
 export class AppModule {}
--- src/modules/Auth/Auth.service.ts
+++ src/modules/Auth/Auth.service.ts
@@ -1,10 +1,30 @@
 import { randomInt } from 'node:crypto';
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Cron } from '@nestjs/schedule';
+import type { Model } from 'mongoose';
+import { OneTimePassword } from '#schemas/MongoDB/OneTimePassword/OneTimePassword.js';
 
 @Injectable()
 export class AuthService {
 	private static OTP_MAX_LENGTH = 999999 as const;
 	private static OTP_MIN_LENGTH = 100000 as const;
+
+	public constructor(
+		@InjectModel(OneTimePassword.name)
+		private readonly oneTimePasswordModel: Model<OneTimePassword>,
+	) {}
+
+	@Cron('* * * * *') // Ejecutar cada minuto.
+	protected async deleteExpiredOneTimePasswords(): Promise<void> {
+		const { deletedCount } = await this.oneTimePasswordModel.deleteMany({
+			expiresIn: {
+				$lt: new Date(),
+			},
+		});
+
+		console.log(`Se han eliminado ${deletedCount} documento(s) expirados`);
+	}
 
 	public generateOneTimePasswordCode(): string {
 		const otpInteger = randomInt(AuthService.OTP_MIN_LENGTH, AuthService.OTP_MAX_LENGTH);
