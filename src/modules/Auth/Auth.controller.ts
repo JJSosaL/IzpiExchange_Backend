4ddc31d08dc8f600d3172435a706bce5a8863557
JJSosaL <hello@fancystudio.xyz>
2026-04-21 22:50:56 +0200

    Añadir ruta para iniciar sesión

--- src/modules/Auth/Auth.controller.ts
+++ src/modules/Auth/Auth.controller.ts
@@ -9,27 +9,30 @@ import {
 	ACCOUNT_WITH_SAME_EMAIL_ALREADY_REGISTERED_RESPONSE,
 	EMAIL_DOMAIN_NOT_ALLOWED_RESPONSE,
 } from '#lib/Responses/Auth.js';
-import { NOT_FOUND_RESPONSE } from '#lib/Responses/Shared.js';
+import { INTERNAL_SERVER_ERROR, NOT_FOUND_RESPONSE } from '#lib/Responses/Shared.js';
 import { EmailService } from '#modules/Email/Email.service.js';
-import { SignUpOtp } from '#mongo/SignUpOtp.js';
-import { User } from '#mongo/User.js';
+import { OneTimePassword } from '#root/schemas/MongoDB/OneTimePassword/OneTimePassword.js';
+import { OneTimePasswordAction } from '#root/schemas/MongoDB/OneTimePassword/OneTimePassword.types.js';
+import { User } from '#schemas/MongoDB/User/User.schema.js';
 import {
+	type SignInDto,
+	SignInSchema,
+	type SignUpDto,
 	SignUpSchema,
-	type SignUpSchemaDto,
-	VerifySignUpOtpSchema,
-	type VerifySignUpOtpSchemaDto,
-} from '#zod/AuthSchema.js';
+	type VerifyOneTimePasswordDto,
+	VerifyOneTimePasswordSchema,
+} from '#schemas/Zod/Auth/Auth.schema.js';
 import { AuthService } from './Auth.service.js';
 
 @Controller('auth')
 export class AuthController {
-	private static OTP_MINUTES = 5 as const;
+	private static OTP_AUTH_MINUTES = 5 as const;
 
 	public constructor(
 		@InjectConnection() private readonly connection: Connection,
 
-		@InjectModel(SignUpOtp.name)
-		private readonly signUpOtpModel: Model<SignUpOtp>,
+		@InjectModel(OneTimePassword.name)
+		private readonly oneTimePasswordModel: Model<OneTimePassword>,
 		@InjectModel(User.name) private readonly userModel: Model<User>,
 
 		@Inject(AuthService) private readonly authService: AuthService,
@@ -37,9 +40,41 @@ export class AuthController {
 		@Inject(JwtService) private readonly jwtService: JwtService,
 	) {}
 
+	@Post('sign-in')
+	@UsePipes(new ZodValidationPipe(SignInSchema))
+	protected async handleSignIn(@Body() signInData: SignInDto) {
+		const { email } = signInData;
+
+		// Verificar que el correo electrónico proviene de un dominio permitido.
+		const isValidEmailDomain = this.emailService.isValidEmailDomain(email);
+
+		if (!isValidEmailDomain) {
+			throw EMAIL_DOMAIN_NOT_ALLOWED_RESPONSE();
+		}
+
+		const otp = this.authService.generateOneTimePasswordCode();
+
+		const otpExpiration = dayjs().add(AuthController.OTP_AUTH_MINUTES, 'minutes');
+		const otpExpirationDate = otpExpiration.toDate();
+
+		await this.oneTimePasswordModel.create({
+			action: OneTimePasswordAction.SignIn,
+			email,
+			expiresIn: otpExpirationDate,
+			otp,
+		});
+
+		await this.emailService.sendSignInMail({
+			otpCode: otp,
+			recipient: email,
+		});
+
+		return {};
+	}
+
 	@Post('sign-up')
 	@UsePipes(new ZodValidationPipe(SignUpSchema))
-	protected async handleSignUp(@Body() signUpData: SignUpSchemaDto) {
+	protected async handleSignUp(@Body() signUpData: SignUpDto) {
 		const { email } = signUpData;
 
 		// Verificar que el correo electrónico proviene de un dominio permitido.
@@ -51,16 +86,17 @@ export class AuthController {
 
 		const otp = this.authService.generateOneTimePasswordCode();
 
-		const otpExpiration = dayjs().add(AuthController.OTP_MINUTES, 'minutes');
+		const otpExpiration = dayjs().add(AuthController.OTP_AUTH_MINUTES, 'minutes');
 		const otpExpirationDate = otpExpiration.toDate();
 
-		await this.signUpOtpModel.create({
+		await this.oneTimePasswordModel.create({
+			action: OneTimePasswordAction.SignUp,
 			email,
 			expiresIn: otpExpirationDate,
 			otp,
 		});
 
-		await this.emailService.sendOneTimePasswordMail({
+		await this.emailService.sendSignUpMail({
 			otpCode: otp,
 			recipient: email,
 		});
@@ -68,12 +104,13 @@ export class AuthController {
 		return {};
 	}
 
-	@Post('sign-up/verify-otp')
-	@UsePipes(new ZodValidationPipe(VerifySignUpOtpSchema))
-	protected async verifySignUpOtp(@Body() verifyOtpData: VerifySignUpOtpSchemaDto) {
-		const { otp } = verifyOtpData;
+	@Post('verify-otp')
+	@UsePipes(new ZodValidationPipe(VerifyOneTimePasswordSchema))
+	protected async verifySignUpOtp(@Body() verifyOtpData: VerifyOneTimePasswordDto) {
+		const { action, otp } = verifyOtpData;
 
-		const otpDocument = await this.signUpOtpModel.findOne({
+		const otpDocument = await this.oneTimePasswordModel.findOne({
+			action,
 			otp,
 		});
 
@@ -88,61 +125,97 @@ export class AuthController {
 		 * eliminamos el documento de la base de datos.
 		 */
 		if (otpDocument.hasAlreadyExpired) {
-			await this.signUpOtpModel.deleteOne({
-				otp,
-			});
-
-			throw NOT_FOUND_RESPONSE();
+			await this.oneTimePasswordModel
+				.deleteOne({
+					action,
+					otp,
+				})
+				.then(() => {
+					throw NOT_FOUND_RESPONSE();
+				});
 		}
 
 		const { email } = otpDocument;
-		const { username } = this.emailService.parseEmail(email);
 
-		const userDocument = await this.userModel.findOne({
-			email,
-		});
+		switch (action) {
+			case OneTimePasswordAction.SignIn: {
+				const userDocument = await this.userModel.findOne({
+					email,
+				});
 
-		/*
-		 * Comprobamos si ya existe una cuenta con el mismo correo electrónico
-		 * asociado.
-		 */
-		if (userDocument) {
-			throw ACCOUNT_WITH_SAME_EMAIL_ALREADY_REGISTERED_RESPONSE();
-		}
+				if (!userDocument) {
+					throw NOT_FOUND_RESPONSE();
+				}
 
-		const userId = randomUUID();
-		const session = await this.connection.startSession();
+				const { id, username } = userDocument;
+				const accessToken = await this.jwtService.signAsync({
+					userId: id,
+					username,
+				});
 
-		await session.withTransaction(async () => {
-			await this.signUpOtpModel.deleteOne(
-				{
+				await this.oneTimePasswordModel.deleteOne({
+					action,
 					otp,
-				},
-				{
-					session,
-				},
-			);
-			await this.userModel.create(
-				[
-					{
-						email,
-						id: userId,
-						username,
-					},
-				],
-				{
-					session,
-				},
-			);
-		});
+				});
 
-		const accessToken = await this.jwtService.signAsync({
-			userId,
-			username,
-		});
+				return {
+					accessToken,
+				};
+			}
+			case OneTimePasswordAction.SignUp: {
+				const userDocument = await this.userModel.findOne({
+					email,
+				});
 
-		return {
-			accessToken,
-		};
+				/*
+				 * Comprobamos si ya existe una cuenta con el mismo correo electrónico
+				 * asociado.
+				 */
+				if (userDocument) {
+					throw ACCOUNT_WITH_SAME_EMAIL_ALREADY_REGISTERED_RESPONSE();
+				}
+
+				const { username } = this.emailService.parseEmail(email);
+
+				const userId = randomUUID();
+				const session = await this.connection.startSession();
+
+				await session.withTransaction(async () => {
+					await this.oneTimePasswordModel.deleteOne(
+						{
+							action,
+							otp,
+						},
+						{
+							session,
+						},
+					);
+					await this.userModel.create(
+						[
+							{
+								email,
+								id: userId,
+								username,
+							},
+						],
+						{
+							session,
+						},
+					);
+				});
+
+				const accessToken = await this.jwtService.signAsync({
+					userId,
+					username,
+				});
+
+				return {
+					accessToken,
+				};
+			}
+			default: {
+				throw INTERNAL_SERVER_ERROR();
+			}
+		}
 	}
 }
--- src/schemas/MongoDB/OneTimePassword/OneTimePassword.types.ts
+++ src/schemas/MongoDB/OneTimePassword/OneTimePassword.types.ts
@@ -5,6 +5,5 @@ export type OneTimePasswordDocument = HydratedDocument<OneTimePassword>;
 
 export enum OneTimePasswordAction {
 	SignIn = 'SIGN_IN',
-	SignOut = 'SIGN_OUT',
 	SignUp = 'SIGN_UP',
 }
