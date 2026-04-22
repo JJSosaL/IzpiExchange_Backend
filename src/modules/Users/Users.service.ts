37daf011d1c14490fefa85f769e88f1a7479008c
JJSosaL <hello@fancystudio.xyz>
2026-04-22 23:11:36 +0200

    Añadir incremento y decremento de créditos

--- src/modules/Users/Users.service.ts
+++ src/modules/Users/Users.service.ts
@@ -7,6 +7,30 @@ import type { UserDocument, UserModel } from '#schemas/MongoDB/User/User.types.j
 export class UsersService {
 	public constructor(@InjectModel(User.name) private readonly usersModel: UserModel) {}
 
+	private async updateUserCredits(userId: string, amount: number): Promise<boolean> {
+		return await this.usersModel
+			.findOneAndUpdate(
+				{
+					id: userId,
+				},
+				{
+					$inc: {
+						credits: amount,
+					},
+				},
+			)
+			.then(() => true)
+			.catch(() => false);
+	}
+
+	public async decrementCredits(userId: string, amount: number): Promise<boolean> {
+		return await this.updateUserCredits(userId, -amount);
+	}
+
+	public async incrementCredits(userId: string, amount: number): Promise<boolean> {
+		return await this.updateUserCredits(userId, amount);
+	}
+
 	public async get(id: string): Promise<UserDocument | null> {
 		return await this.usersModel.findOne({
 			id,
