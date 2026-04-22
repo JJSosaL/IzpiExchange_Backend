9b01245f810b3cb73827ce76049ef01b5ca367bb
JJSosaL <hello@fancystudio.xyz>
2026-04-22 20:25:01 +0200

    Añadir esquemas a la creación de productos

---
+++ src/schemas/Zod/Product/Product.messages.ts
@@ -0,0 +1,18 @@
+export const PRODUCT_DESCRIPTION_MUST_BE_STRING =
+	'La descripción del producto debe ser una cadena de texto' as const;
+export const PRODUCT_DESCRIPTION_MUST_HAVE_MAX_LENGTH = (maxLength: number) =>
+	`La descripción del producto debe tener ${maxLength} caracteres como máximo` as const;
+export const PRODUCT_DESCRIPTION_MUST_HAVE_MIN_LENGTH = (minLength: number) =>
+	`La descripción del producto debe tener ${minLength} caracteres como mínimo` as const;
+
+export const PRODUCT_NAME_MUST_BE_STRING =
+	'El nombre del producto debe ser una cadena de texto' as const;
+export const PRODUCT_NAME_MUST_HAVE_MAX_LENGTH = (maxLength: number) =>
+	`El nombre del producto debe tener ${maxLength} caracteres como máximo` as const;
+export const PRODUCT_NAME_MUST_HAVE_MIN_LENGTH = (minLength: number) =>
+	`El nombre del producto debe tener ${minLength} caracteres como mínimo` as const;
+
+export const PRODUCT_PRICE_MUST_BE_INTEGER =
+	'El precio del producto debe ser un número entero' as const;
+export const PRODUCT_PRICE_MUST_BE_POSITIVE =
+	'El precio del producto debe ser un número positivo' as const;
