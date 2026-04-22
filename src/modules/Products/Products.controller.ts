ab862e20bdda5258407c62d94716c758b6a9e439
JJSosaL <hello@fancystudio.xyz>
2026-04-22 23:28:27 +0200

    Añadir lista de productos

--- src/modules/Products/Products.controller.ts
+++ src/modules/Products/Products.controller.ts
@@ -1,5 +1,5 @@
 import { randomUUID } from 'node:crypto';
-import { Body, Controller, Inject, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Inject, Post, UseGuards } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { User } from '#common/Decorators/User.decorator.js';
 import { AuthGuard } from '#common/Guards/Auth.guard.js';
@@ -7,13 +7,13 @@ import { ZodValidationPipe } from '#common/Pipes/ZodValidation.pipe.js';
 import { USER_MUST_HAVE_HIGHER_BALANCE_THAN_PRICE_RESPONSE } from '#lib/Responses/Products.js';
 import { INTERNAL_SERVER_ERROR_RESPONSE } from '#lib/Responses/Shared.js';
 import { UsersService } from '#modules/Users/Users.service.js';
-import type { ProductModel } from '#root/schemas/MongoDB/Product/Product.types.js';
-import type { UserDocument } from '#root/schemas/MongoDB/User/User.types.js';
 import {
 	type ProductCreateDto,
 	ProductCreateSchema,
 } from '#root/schemas/Zod/Product/Product.schema.js';
 import { Product } from '#schemas/MongoDB/Product/Product.schema.js';
+import { type ProductModel, ProductStatus } from '#schemas/MongoDB/Product/Product.types.js';
+import type { UserDocument } from '#schemas/MongoDB/User/User.types.js';
 
 @Controller('products')
 @UseGuards(AuthGuard)
@@ -22,6 +22,18 @@ export class ProductsController {
 		@InjectModel(Product.name) private readonly productModel: ProductModel,
 		@Inject(UsersService) private readonly usersService: UsersService,
 	) {}
+
+	@Get()
+	protected async handleGet() {
+		return await this.productModel
+			.find({
+				status: ProductStatus.Published,
+			})
+			// Excluimos los campos por defecto de MongoDB.
+			.select('-_id -__v')
+			// Excluimos los campos que no son necesarios.
+			.select('-createdAt -status -updatedAt');
+	}
 
 	@Post('publish')
 	protected async handlePublish(
